bb45f390073b800d7a5a42259e4fe28897557050
FeliceDiCasola <felicedicasola3@gmail.com>
2026-09-07 09:45:01 +0200

    Update run_deseq2.R

--- bin/run_deseq2.R
+++ bin/run_deseq2.R
@@ -163,14 +163,17 @@ for (pair in contrast_pairs) {
     abline(h=-log10(user_pvalue), col="blue", lty=2)
     dev.off()
     
-# 4. Heatmap Top 50 
+  # 4. Heatmap Top 50
     top_genes <- head(order(res$padj), 50)
     if (length(top_genes) > 1) {
         mat <- assay(vsd)[top_genes, ]
         mat <- mat - rowMeans(mat)
-        colori_heatmap <- colorRampPalette(c("blue", "white", "red"))(256)
-    
-        heatmap(mat, scale="none", col=colori_heatmap, margins=c(12, 12), cexCol=0.5, cexRow=0.5, main=paste("Heatmap Top 50:", c_name))
+       
+        pheatmap::pheatmap(mat,
+                           scale = "none",
+                           main = paste("Heatmap Top 50:", c_name),
+                           fontsize_row = 6, 
+                           fontsize_col = 10)
     }
     
     # 5. Top 6 geni (specifici per il contrasto)
